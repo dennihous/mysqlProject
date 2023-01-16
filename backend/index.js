3545564fbe7da9d1da59bfc513204466b38180cf
dennis <denhouston1@icloud.com>
2023-01-16 14:00:00 +0100

    Added db connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 import express from "express"
+import mysql from "mysql"
 
 const app = express()
+const db = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"test"
+})
 
 app.listen(8800, () => {
   console.log("Connected to port 8800")
