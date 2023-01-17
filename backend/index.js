e9c689d748e9457a3f5b6dc021c58ddd2246dce4
dennis <denhouston1@icloud.com>
2023-01-17 22:30:13 +0000

    Added sql connection for books

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,17 @@ app.get("/", (req, res) => {
   res.json("This is the backend")
 })
 
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books"
+  db.query(q, (err, data) => {
+    if(err){
+      return res.json(err)
+    } else {
+      return res.json(data)
+    }
+  })
+})
+
 app.listen(8800, () => {
   console.log("Connected to port 8800")
 })
