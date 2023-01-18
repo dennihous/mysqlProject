1fe3fae83d0422eebf451c2fa9c492a4962e5541
dennis <denhouston1@icloud.com>
2023-01-18 21:15:01 +0000

    Added use of express in client

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
   database:"test",
 })
 
-// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY process.env.PASSWORD  
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.json("This is the backend")
@@ -31,7 +31,11 @@ app.get("/books", (req, res) => {
 
 app.post("/books", (req, res) => {
   const q = "INSERT INTO books (`title`, `description`, `cover`) VALUES (?)"
-  const values = ["title from backend", "description from backend", "cover from backend"]
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.cover,
+  ]
 
   db.query(q, [values], (err, data) => {
     if(err){
