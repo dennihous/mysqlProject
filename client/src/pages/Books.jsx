0a0656aeb25ff09f336fa33446d223b45d54b898
dennis <denhouston1@icloud.com>
2023-01-19 19:43:29 +0000

    Added link to add page

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 const Books = () => {
 
@@ -31,6 +32,7 @@ const Books = () => {
           </div>
         })}
       </div>
+      <button><Link to="/add" >Add new book</Link></button>
     </div>
   )
 }
