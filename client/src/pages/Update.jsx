bb533c9df412ffbcf9a261ca31835ff33d3ef8b0
dennis <denhouston1@icloud.com>
2023-01-19 22:36:53 +0000

    Update page utilising add page

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,8 +1,45 @@
+import axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Update = () => {
+  const [book, setBook] = useState({
+    title: "",
+    description: "",
+    price: null,
+    cover: ""
+  })
+
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setBook(prev => ({
+      ...prev, [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault()
+    try{
+      await axios.post("http://localhost:8800/books", book)
+      navigate("/")
+    } catch(err){
+      console.log(err)
+    }
+  }
+
+  console.log(book)
+
   return (
-    <div>Update</div>
+    <div className='form' >
+      <h1>Update Book</h1>
+      <input type="text" placeholder="title" onChange={handleChange} name="title" />
+      <input type="text" placeholder="description" onChange={handleChange} name="description" />
+      <input type="number" placeholder="price" onChange={handleChange} name="price" />
+      <input type="text" placeholder="cover" onChange={handleChange} name="cover" />
+      <button onClick={handleClick}>Update</button>
+    </div>
   )
 }
 
