bd5437fd69f9ec0bed2394d8133a7ce0335224bc
dennis <denhouston1@icloud.com>
2023-01-19 22:47:26 +0000

    Added update query

--- backend/index.js
+++ backend/index.js
@@ -62,6 +62,26 @@ app.delete("/books/:id", (req, res) => {
   })
 })
 
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE books SET `title` = ?, `description` = ?, `price` = ?, `cover` = ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.price,
+    req.body.cover,
+  ] 
+
+  db.query(q, [...values, bookId], (err, data) => {
+    if(err){
+      return res.json(err)
+    } else {
+      return res.json("Book has been updated successfully")
+    }
+  })
+})
+
 app.listen(8800, () => {
   console.log("Connected to port 8800")
 })
