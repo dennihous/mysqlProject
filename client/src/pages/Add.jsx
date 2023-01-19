168a95209e1c164245b4fe27820209459f44cb5a
dennis <denhouston1@icloud.com>
2023-01-19 20:06:00 +0000

    Added inputs for new book

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Add = () => {
+
   return (
-    <div>Add</div>
+    <div className='form' >
+      <h1>Add New Book</h1>
+      <input type="text" placeholder="title" />
+      <input type="text" placeholder="description" />
+      <input type="number" placeholder="price" />
+      <input type="text" placeholder="cover" />
+    </div>
   )
 }
 
