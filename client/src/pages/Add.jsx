f7883746b796a96cf43d85d8d4bc598f8a7ac62c
dennis <denhouston1@icloud.com>
2023-01-19 20:23:53 +0000

    handleChange implemented

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -8,14 +8,20 @@ const Add = () => {
     price: null,
     cover: ""
   })
-  
+
+  const handleChange = (e) => {
+    setBook(prev => ({
+      ...prev, [e.target.name]: e.target.value
+    }))
+  }
+
   return (
     <div className='form' >
       <h1>Add New Book</h1>
-      <input type="text" placeholder="title" />
-      <input type="text" placeholder="description" />
-      <input type="number" placeholder="price" />
-      <input type="text" placeholder="cover" />
+      <input type="text" placeholder="title" onChange={handleChange} name="title" />
+      <input type="text" placeholder="description" onChange={handleChange} name="description" />
+      <input type="number" placeholder="price" onChange={handleChange} name="price" />
+      <input type="text" placeholder="cover" onChange={handleChange} name="cover" />
     </div>
   )
 }
